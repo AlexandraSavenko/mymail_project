33219d3850b0e99d7f3b77a08f33b50964de1f1d
Sasha Savenko <sashasavenko3@gmail.com>
2025-08-06 16:09:24 +0300

    productsPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,11 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
+import Products from "./pages/products/Products";
 
-interface Product {
-        id: number,
-        title: string,
-        price: number,
-        description: string,
-        category: string,
-        image: string,
-        rating: {
-            rate: number,
-            count: number
-        }
-}
+
+
 function App() {
-  const [products, setProducts] = useState<Product[]>([]);
-  const [filter, setFilter] = useState('')
-  const [ascending, setAscending] = useState(true) 
-  const [page, setPage] = useState(1)
-  const [totalPages, setTotalPages] = useState(0)
-  const perPage = 5;
-  const startIndex = (page - 1) * perPage;
-  const endIndex = startIndex + perPage;
-useEffect(()=>{
-  const getProducts = async () => {
-     try {
-      const response = await axios.get("https://fakestoreapi.com/products");
-setProducts(response.data) 
-setTotalPages(Math.ceil(response.data.length / 5))  
-    } catch (error) {
-        console.error(error)
-    }
-  }
- getProducts()
-},[])
-if(products.length === 0){
-  return <p>no products were fetched</p>
-}
-
-  const visibleProducts = filter ? products.filter(product => product.title.toLowerCase().startsWith(filter.toLowerCase())) : products;
- const visibleProductsOrder = visibleProducts.slice(startIndex, endIndex).sort((a, b) => ascending ? a.price - b.price : b.price - a.price)
+  
  return <div>
-<h1>Products</h1>
-<button onClick={() => setAscending(!ascending)}>{ascending ? "Show the highest prices": "Show the lowest prices"}</button>
-<input type="text" value={filter} onChange={e => {setFilter(e.target.value); setPage(1)}} />
-<ul>
-{visibleProductsOrder.map(product => <li key={product.id}> <p>{product.title}</p> <p>{product.price}</p> </li>) }</ul>
-<button disabled={page === 1} onClick={() => {setPage(page + 1); }}>Next</button>
-<button disabled={page === totalPages} onClick={() => {setPage(page - 1); }}>Back</button>
-{totalPages && <p>{`page ${page} of ${totalPages}`}</p> }
+<Products/>
   </div>
   }
 
---
+++ src/pages/products/Products.tsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from "react";
+import { fetchData } from "../../utils/operation";
+
+interface Product {
+        id: number,
+        title: string,
+        price: number,
+        description: string,
+        category: string,
+        image: string,
+        rating: {
+            rate: number,
+            count: number
+        }
+}
+const Products = () => {
+    const [products, setProducts] = useState<Product[]>([]);
+  const [filter, setFilter] = useState('')
+  const [ascending, setAscending] = useState(true) 
+  const [page, setPage] = useState(1)
+  const [totalPages, setTotalPages] = useState(0)
+  const perPage = 5;
+  const startIndex = (page - 1) * perPage;
+  const endIndex = startIndex + perPage;
+useEffect(()=>{
+  const getProducts = async () => {
+     try {
+      const response = await fetchData()
+setProducts(response) 
+setTotalPages(Math.ceil(response.length / 5))  
+    } catch (error) {
+        console.error(error)
+    }
+  }
+ getProducts()
+},[])
+if(products.length === 0){
+  return <p>no products were fetched</p>
+}
+
+  const filteredProducts = filter ? products.filter(product => product.title.toLowerCase().startsWith(filter.toLowerCase())) : products;
+ const sortedProducts = filteredProducts.sort((a, b) => ascending ? a.price - b.price : b.price - a.price)
+ const pageOfProducts = sortedProducts.slice(startIndex, endIndex)
+    return <div>
+        <h2>Products</h2>
+<button onClick={() => setAscending(!ascending)}>{ascending ? "Show the highest prices": "Show the lowest prices"}</button>
+<input type="text" value={filter} onChange={e => {setFilter(e.target.value); setPage(1)}} />
+<ul>
+{pageOfProducts.map(product => <li key={product.id}> <p>{product.title}</p> <p>{product.price}</p> </li>) }</ul>
+<button disabled={page === 1} onClick={() => {setPage(page - 1); }}>Back</button>
+<button disabled={page === totalPages} onClick={() => {setPage(page + 1); }}>Next</button>
+{totalPages && <p>{`page ${page} of ${totalPages}`}</p> }
+    </div>
+}
+
+export default Products;
